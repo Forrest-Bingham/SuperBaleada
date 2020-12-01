ea68ca86def798a19bb9a47d354644c9077f81c4
Forrest Bingham <bingham_5@live.com>
2020-11-30 17:25:48 -0700

    Set up home page, working on Menu

---
+++ superbaleada/src/components/Cart.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./navbar";
+
+const Cart = () => {
+
+    return(
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Cart;
